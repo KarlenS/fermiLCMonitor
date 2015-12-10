dc9403355c980d5b296d499cf9e857e0b0095bd8
KarlenS <shkarlen@gmail.com>
2015-12-10 16:16:16 -0600

    can grab fits for the light curves of all the source, but no analysis capabilities yet

--- fermiLCMonitor.py
+++ fermiLCMonitor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
-import urllib2
+import urllib2,requests
 import argparse
 from astropy.io import fits as pf
+import numpy as np
 from bs4 import BeautifulSoup
 from time import gmtime, strftime
+import matplotlib.pyplot as plt
 
 class fermiLC(object):
 
@@ -16,10 +18,8 @@ class fermiLC(object):
         fermiURL = 'http://fermi.gsfc.nasa.gov/ssc/data/access/lat/msl_lc/'
         return urllib2.urlopen(fermiURL)
 
-    #def getAllLinks(self)
-
-    def downloadLCfits(self, url):
-        fitsfile = wget.download(url)
+    def getLCfits(self, url):
+        fitsfile = pf.open(url)
         return fitsfile
 
     #def calcBaseline(self):
@@ -45,20 +45,31 @@ def main():
 
     for link in soup.find_all('a'):
         if link.get_text() == 'Daily Light Curve Fits File':
-            print 'found daily'
-            #url = linkbase + link.get('href'))
-            #lcDailyFile = lc.downloadLCfits(url)
-            #need to break things out for each source
+            url = linkbase + link.get('href')
+            lcDailyFile = lc.getLCfits(url)
+
+            #opening up the fits file
+            data_daily = lcDailyFile[1].data
+
+            #grab the light curve
+            start_time = data_daily['START']
+            end_time = data_daily['STOP']
+            flux_100_300000 = data_daily['FLUX_100_300000']
+            error_100_300000 = data_daily['ERROR_100_300000']
+
+            #calculate light curve parameters
+            weights = np.divide(1.,error_100_300000)
+            print data_daily['NAME'][0],np.average(flux_100_300000,weights=weights)
+
+            #plotting the light curve,,,
+            plt.errorbar(start_time,flux_100_300000,yerr=error_100_300000,fmt='o')
+            plt.show()
+
+            break
+           
         if link.get_text() == 'Weekly Light Curve Fits File':
-            #url = linkbase + link.get('href'))
-            #lcWeeklyfile = lc.downloadLCfits(url)
-            print 'found weekly'
+            url = linkbase + link.get('href')
+            lcWeeklyfile = lc.getLCfits(url)
        
-       # if (link.get('href')[0:6]) == 'source':
-       #     print "Source: ", link.get('href')[7:]
-        #check to see if the source
-        #if so grab the 
-
-
 if __name__ == '__main__':
     main()
